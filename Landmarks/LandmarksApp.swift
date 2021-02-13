11c4f463e50df9944110b2571583c81861d4ffdb
Matheus Henrick Dias <matheushd@ciandt.com>
2021-02-13 15:19:41 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matheus Henrick Dias on 13/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
